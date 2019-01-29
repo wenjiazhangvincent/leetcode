1f4e634d8c5ba39a2f0f6bba728c1054daf89d87
wenjiazhangvincent <wenjiazhang1127@gmail.com>
2019-01-29 21:07:01 +0800

    last in Netease

--- 0.py
+++ 0.py
@@ -1,2 +1,9 @@
-x = str(122)
-print x[0]
+
+lst = range(22)
+for i in xrange(len(lst)):
+    lst[i] += 1
+    if lst[i] < 10:
+        lst[i] = '0' + str(lst[i])
+    else:
+        lst[i] = str(lst[i])
+print lst
